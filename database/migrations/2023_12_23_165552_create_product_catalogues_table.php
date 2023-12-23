319023457126084dbb51de0cba9ceb36563b5551
quangphuc123 <0306201369@caothang.edu.vn>
2023-12-24 00:33:31 +0700

    thêm 2 bảng sản phẩm và danh mục sản phẩm

---
+++ database/migrations/2023_12_23_165552_create_product_catalogues_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_catalogues', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->tinyInteger('publish')->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_catalogues');
+    }
+};

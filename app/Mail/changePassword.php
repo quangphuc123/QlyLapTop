82696c0cdd5d27479694cdb3b769a6419c1bc27e
TruongDucQuyen <0306201374@caothang.edu.vn>
2024-01-04 23:03:28 +0700

    update changePassword

---
+++ app/Mail/changePassword.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class changePassword extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            replyTo: mb_encode_mimeheader(env('MAIL_FROM_ADDRESS')),
+            subject: 'Quên mật khẩu',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.forgot-email',
+        );
+    }
+
+}

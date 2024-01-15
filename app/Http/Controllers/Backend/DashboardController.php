3a9ac6745052b608767c0e8d8e68e4aa9709c5ae
quangphuc123 <0306201369@caothang.edu.vn>
2024-01-15 18:18:59 +0700

    Update Readme.md

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Order;
+use App\Models\ProductCatalogue;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -14,13 +17,37 @@ class DashboardController extends Controller
     public function index()
     {
         $config = $this->config();
+        $productCatalogue = new ProductCatalogue();
+        $tableColumns = $productCatalogue->getConnection()->getSchemaBuilder()->getColumnListing($productCatalogue->getTable());
+        $result = ProductCatalogue::select('product_catalogues.*', DB::raw('COUNT(products.product_catalogue_id) AS number_cate'))
+            ->join('products', 'product_catalogues.id', '=', 'products.product_catalogue_id')
+            ->groupBy(array_merge(['product_catalogues.id'], $tableColumns))
+            ->get();
 
+        $User = new User();
+        $tableColumnUsers = $User->getConnection()->getSchemaBuilder()->getColumnListing($User->getTable());
+
+        $resultUser = DB::table('user_catalogues')
+            ->select('user_catalogues.*', DB::raw('COUNT(users.user_catalogue_id) AS number_user'))
+            ->join('users', 'user_catalogues.id', '=', 'users.user_catalogue_id')
+            ->groupBy(array_merge(['user_catalogues.id'], $tableColumnUsers))
+            ->get();
+        $totalOrders = Order::count();
+        $totalUsers = User::count();
+        $orders = Order::latest()->limit(5)->get();
+        $totalIncome = Order::sum('order_total');
         $template = 'admin.dashboard.home.index';
         return view(
             'admin.dashboard.admin-layout',
             compact(
                 'template',
-                'config'
+                'config',
+                'result',
+                'resultUser',
+                'totalOrders',
+                'totalUsers',
+                'orders',
+                'totalIncome'
             )
         );
     }
